e095185f8648ffeaf03b17082ca7fd7dbd5b95f6
OmerGery <omergery1996@gmail.com>
2021-07-31 21:12:12 +0300

    more progres

--- app.test.js
+++ app.test.js
@@ -2,7 +2,7 @@
 const k_GooglePageLink = "https://www.google.co.il/search?q=google+calculator";
 const k_PlusClass = "PaQdxb mF5fo";
 const k_EqualClass = "XRsWPe UUhRt";
-const k_NumbersClassTag = "XRsWPe AOvabd";
+const k_NumbersClass = "XRsWPe AOvabd";
 const k_ResultClassTag = "qv3Wpe";
 const { conditionalExpression } = require("@babel/types");
 const { Builder, By, Key, util, Button } = require("selenium-webdriver");
@@ -15,32 +15,41 @@ class GooglePage {
     let driver = await new Builder().forBrowser("chrome").build();
     await driver.get(k_GooglePageLink);
     let NumbersButtons = await driver.findElements(
-      By.className(k_NumbersClassTag)
+      By.className(k_NumbersClass)
     );
     let rightButtons = await driver.findElements(By.className(k_PlusClass));
     let plus = rightButtons[5];
     let equalClass = await driver.findElements(By.className(k_EqualClass));
     let equal = equalClass[0];
+    let allbuttons = await driver.findElements(By.tagName("td"));
     let resultCollection = await driver.findElements(
       By.className(k_ResultClassTag)
     );
     let result = resultCollection[0];
-
     let calc = await new GooglePage(
       driver,
       NumbersButtons,
       result,
       plus,
-      equal
+      equal,
+      allbuttons
     );
     return calc;
   }
-  constructor(driver, numbersButtons, result, additionButton, equalButton) {
+  constructor(
+    driver,
+    numbersButtons,
+    result,
+    additionButton,
+    equalButton,
+    buttonsAsText
+  ) {
     this._driver = driver;
     this._result = result;
     this._numbersButtons = numbersButtons;
     this._additionButton = additionButton;
     this._equalButton = equalButton;
+    this._buttonsAsText = buttonsAsText;
   }
 
   async enterNumber(buttonNum) {
@@ -70,36 +79,40 @@ class GoogleTests {
   }
   static async buttonCountTest() {
     let Page = await GooglePage.CreatePage();
-    let allButtonsInCalc = Page._numbersButtons;
-    let numbersButtons = 0;
-    let amountOfButtons = allButtonsInCalc.length;
-    console.log(amountOfButtons);
+    let allbuttons = Page._buttonsAsText;
+    let amountOfNumberedButtons = 0;
+    let amountOfButtons =
+      Page._buttonsAsText.length - Page._numbersButtons.length;
+    let res = "The amount of buttons is: " + amountOfButtons;
     for (let i = 0; i < amountOfButtons; i++) {
-      console.log(await allButtonsInCalc[i].getText());
+      let currentText = await allbuttons[i].getText();
+      if (currentText >= "0" && currentText <= "9") {
+        amountOfNumberedButtons++;
+      }
     }
-    return numbersButtons.toString();
+    res += "The amount of numbered buttons is: " + amountOfNumberedButtons;
+    await Page._driver.quit();
+    return res;
   }
 }
-GoogleTests.buttonCountTest();
 //jest max time:
-// const jestTimeOutTime = 30000;
-// jest.setTimeout(jestTimeOutTime);
+const jestTimeOutTime = 30000;
+jest.setTimeout(jestTimeOutTime);
 
-// //test 1 consts:
-// const num1 = 5;
-// const num2 = 10;
-// const sum = num1 + num2;
-// // the tests:
-// test(
-//   "Test 1: Addition of 2 numbers: " + num1 + " + " + num2 + " = " + sum,
-//   async () => {
-//     const result = await GoogleTests.Additiontest(num1, num2);
-//     expect(result).toBe(sum);
-//   }
-// );
+//test 1 consts:
+const num1 = 5;
+const num2 = 10;
+const sum = num1 + num2;
+// the tests:
+test(
+  "Test 1: Addition of 2 numbers: " + num1 + " + " + num2 + " = " + sum,
+  async () => {
+    const result = await GoogleTests.Additiontest(num1, num2);
+    expect(result).toBe(sum);
+  }
+);
 
-// test("Button Counting Test:", async () => {
-//   const buttonsAmounts = await GoogleTests.buttonCountTest();
-//   console.log(buttonsAmounts);
-//   expect(5).toBe(5);
-// });
+test("Button Counting Test:", async () => {
+  const outputString = await GoogleTests.buttonCountTest();
+  console.log(outputString);
+});
